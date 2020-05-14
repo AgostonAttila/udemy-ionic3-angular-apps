6e7e15ed843ce9c0914ed14cc1770b4290b6967c
AgostonAttila <atesz014@gmail.com>
2020-05-14 22:51:57 +0200

    20200514

--- Travel app/src/app/places/offers/offers.page.ts
+++ Travel app/src/app/places/offers/offers.page.ts
@@ -8,17 +8,24 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
-  styleUrls: ['./offers.page.scss']
+  styleUrls: ['./offers.page.scss'],
 })
 export class OffersPage implements OnInit, OnDestroy {
   offers: Place[];
+  isLoading = false;
   private placesSub: Subscription;
 
   constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
-    this.placesSub = this.placesService.places.subscribe(places => {
+    this.placesSub = this.placesService.places.subscribe((places) => {
       this.offers = places;
+    });
+  }
+  ionViewWillEnter() {
+    this.isLoading = true;
+    this.placesService.fetchPlaces().subscribe(() => {
+      this.isLoading = false;
     });
   }
 
