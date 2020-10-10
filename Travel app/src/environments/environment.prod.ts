6cf7abf87d80f839b62502a699b1ec79284e62b6
Atesz014 <atesz014@gmail.com>
2020-10-10 14:46:27 +0200

    Update environment.prod.ts

--- Travel app/src/environments/environment.prod.ts
+++ Travel app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: 'AIzaSyB3P1U1KZYcRvql3VMXl2-g5GQlaK6AVlQ',
-  firebaseAPIKey: 'AIzaSyBQUxkz8ADpx504hz9HuyBS62XwB1iU_So',
+  googleMapsAPIKey: 'YOUR KEY',
+  firebaseAPIKey: 'YOUR KEY',
 };
